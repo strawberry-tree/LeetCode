26a37316ad44744f02f3a84206ca89591ddbc76d
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-23 14:43:55 +0900

    Time: 2642 ms (5%), Space: 353.7 MB (5.08%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,23 @@
+class Solution:
+
+
+    
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # key: 숫자값, value: 해당값 마지막 인덱스
+        nums.sort()
+
+        sum_dict = dict()
+        for i, value in enumerate(nums):
+            sum_dict[value] = i
+
+        N = len(nums)
+        answer = []
+        for i in range(N - 2):
+            for j in range(i + 1, N - 1):
+                rest = 0 - (nums[i] + nums[j])
+                
+                if rest in sum_dict and j < sum_dict[rest]:
+                    answer.append((nums[i], nums[j], rest))
+
+        answer = [list(tup) for tup in set(answer)]
+        return answer
