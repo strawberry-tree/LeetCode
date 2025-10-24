7e67b9aea2de73b2ed4388b35d1287c0d1ffa37e
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-24 15:09:10 +0900

    Time: 49 ms (96.75%), Space: 59.8 MB (26.47%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,19 +1,10 @@
+from itertools import combinations
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         answer = []
 
-        # start 이상부터 고를 수 있음. 현재 고른 숫자는 curr에 있음
-        
-        def pick(start, curr):
-            if len(curr) >= k:
-                answer.append(curr[:])
-                return
+        for cmb in combinations(range(1, n + 1), k):
+            answer.append(list(cmb))
 
-            if start > n:
-                return
-            
-            for i in range(start, n + 1):
-                pick(i + 1, curr + [i])
-
-        pick(1, [])
         return answer
