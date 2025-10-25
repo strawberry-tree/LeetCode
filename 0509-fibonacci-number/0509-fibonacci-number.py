52a76698b08d22dd1c542a7e66217e6b00f21234
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-25 14:02:44 +0900

    Time: 20 ms (99.37%), Space: 17.5 MB (89.21%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0] * (n + 1)
+
+        for i in range(n + 1):
+            if i == 0:
+                dp[i] = 0
+            elif i == 1:
+                dp[i] = 1
+            else:
+                dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
