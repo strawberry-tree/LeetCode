c03248ab6540e185af12becfd71cc9d33df57245
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-26 19:28:59 +0900

    Time: 275 ms (62.94%), Space: 18.1 MB (25.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,35 +2,18 @@ from collections import defaultdict
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        palindromes = defaultdict(list)
-        N = len(s)
-        answer = ""
+        def check(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
 
-        for i in range(N):
-            window = 0
-
-            while 0 <= i - window and i + window < N:
-                if s[i - window] != s[i + window]:
-                    break
-                window += 1
-  
-            window -= 1
+        if len(s) < 2:
+            return s
             
-            if window >= 0 and 1 + 2 * window > len(answer):
-                print(answer, s[i-window:i+window+1])
-                answer = s[i - window:i+window + 1]
-
-        for i in range(N - 1):
-            window = 0
-            while 0 <= i - window and i + window + 1 < N:
-                if s[i - window] != s[i + window + 1]:
-                    break
-                window += 1
-            window -= 1
-
-            if window >= 0 and 2 * window + 2 > len(answer):
-                print(answer, s[i-window:i+window+2])
-                answer = s[i - window: i+ window + 2]
+        answer = ""
+        for i in range(len(s) - 1):
+            answer = max(answer, check(i, i + 1), check(i, i + 2), key=len)
 
         return answer 
             
