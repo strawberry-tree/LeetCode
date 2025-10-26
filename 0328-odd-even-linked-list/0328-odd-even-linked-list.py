012809ec07b7f9fdb6a18f83e450d2f529dd10b0
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-26 20:18:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        # 짝수노드 배치용 샌티넬
+        evenHead = ListNode()
+
+        # curr은 무조건 홀수번째 노드
+        while curr:
+            if not curr.next:
+                prev = curr
+                break
+            
+            evenOut = curr.next
+            curr.next = curr.next.next
+
+            if curr.next is None:
+                prev = curr
+            curr = curr.next
+            
+        
+            evenCurr = evenHead
+            while evenCurr and evenCurr.next:
+                evenCurr = evenCurr.next
+            evenCurr.next = evenOut
+            evenOut.next = None #!!
+
+        prev.next = evenHead.next
+        
+        return head
+
+        
+
+
+
+            
+            
+            
