5cc68a1082b7b02299fd48c330a855daa86a4075
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-26 20:22:29 +0900

    Time: 0 ms (100%), Space: 19.2 MB (74.05%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,33 +5,18 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = head
-        prev = None
-        # 짝수노드 배치용 샌티넬
-        evenHead = ListNode()
-
-        # curr은 무조건 홀수번째 노드
-        while curr:
-            if not curr.next:
-                prev = curr
-                break
-            
-            evenOut = curr.next
-            curr.next = curr.next.next
-
-            if curr.next is None:
-                prev = curr
-            curr = curr.next
-            
+        if head is None:
+            return None
         
-            evenCurr = evenHead
-            while evenCurr and evenCurr.next:
-                evenCurr = evenCurr.next
-            evenCurr.next = evenOut
-            evenOut.next = None #!!
+        odd = head
+        even = head.next
+        even_head = head.next
 
-        if prev:
-            prev.next = evenHead.next
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        odd.next = even_head
         
         return head
 
